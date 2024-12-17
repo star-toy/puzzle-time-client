2116c8dd32123bd87cc0e0015b840ef90d567af3
Kay Cha <kay@hannal.net>
2024-12-17 17:10:55 +0900

    FIX 메인 페이지 layout

--- app/(route)/main/layout.tsx
+++ app/(route)/main/layout.tsx
@@ -4,18 +4,29 @@ import Image from 'next/image';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="w-full h-full bg-[url('/assets/mainpage/christmas/christmas-bg-frame.png')] bg-cover bg-center">
-      <div>
-        <Image
-          src="/assets/mainpage/christmas/ceilling-deco.png"
-          alt=""
-          width={1920}
-          height={120}
-          className="absolute top-0 left-0"
-        />
-        Puzzle Time (feat. PuddingCamp)
-        {children}
-      </div>
+    <div className="relative w-full h-full bg-[url('/assets/mainpage/christmas/background.png')] bg-cover bg-center">
+      <Image
+        src="/assets/mainpage/christmas/christmas-bg-frame.png"
+        alt=""
+        width={1920}
+        height={1080}
+        className="absolute z-50 top-0 left-0 w-auto h-auto bg-cover bg-center"
+      />
+      <Image
+        src="/assets/mainpage/christmas/ceilling-deco.png"
+        alt=""
+        width={1920}
+        height={120}
+        className="absolute z-[60] top-0 left-0 w-auto h-auto bg-cover bg-center"
+      />
+      <Image
+        src="/assets/mainpage/christmas/ceilling-lights.png"
+        alt=""
+        width={1920}
+        height={20}
+        className="absolute z-[70] top-0 left-0 w-auto h-auto bg-cover bg-center"
+      />
+        <div className="w-full h-full pt-[162px] px-[130px] pb-[35px]">{children}</div>
     </div>
   );
 }
