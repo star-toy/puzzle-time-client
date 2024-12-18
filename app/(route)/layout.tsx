0ac62d3804251a214237113621f8bd367cdbaec0
Kay Cha <kay@hannal.net>
2024-12-18 18:26:04 +0900

    FIX 이중 zooming 제거

--- app/(route)/layout.tsx
+++ app/(route)/layout.tsx
@@ -4,6 +4,8 @@ import { useEffect } from 'react';
 
 import { SCREEN_WIDTH } from '../constants';
 
+import { debounce } from '@/app/_utils/debounce';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ZoomHandler>
@@ -14,12 +16,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
 function ZoomHandler({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    const handleResize = () => {
+    const handleResize = debounce(() => {
       const width = window.innerWidth;
       const scale = width / SCREEN_WIDTH;
       document.body.style.transform = `scale(${scale})`;
       document.body.style.transformOrigin = 'top left';
-    };
+    }, 200);
 
     handleResize();
     window.addEventListener('resize', handleResize);
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,8 @@
-'use client';
-
-import React, { useEffect } from 'react';
-
-import { SCREEN_WIDTH } from './constants';
+import React from 'react';
 
 import '@/app/_styles/globals.css';
 
 import ReactQueryDevToolsProvider from '@/app/_components/_providers/ReactQueryProvider';
-import { debounce } from '@/app/_utils/debounce';
-
-function ZoomHandler({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    const handleResize = debounce(() => {
-      const width = window.innerWidth;
-      const scale = width / SCREEN_WIDTH;
-      document.body.style.zoom = scale.toString();
-    }, 200); // 200ms 딜레이
-
-    // 초기 로드 시 바로 적용 (debounce 없이)
-    const width = window.innerWidth;
-    const scale = width / SCREEN_WIDTH;
-    document.body.style.zoom = scale.toString();
-
-    // resize 이벤트에는 debounce 적용
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  return children;
-}
 
 export default function RootLayout({
   children,
@@ -39,9 +12,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
-        <ReactQueryDevToolsProvider>
-          <ZoomHandler>{children}</ZoomHandler>
-        </ReactQueryDevToolsProvider>
+        <ReactQueryDevToolsProvider>{children}</ReactQueryDevToolsProvider>
       </body>
     </html>
   );
