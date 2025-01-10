545c98e37f244bda7fd55757144e2197d61e7412
Kay Cha <kay@hannal.net>
2025-01-10 13:13:33 +0900

    WIP

--- app/(route)/layout.tsx
+++ app/(route)/layout.tsx
@@ -1,4 +1,6 @@
+import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
+import type { Session } from 'next-auth';
 
 import ResizeLayout from '../_components/_layouts/resize-layout';
 import BGMProvider from '../_components/bgm-provider';
@@ -7,15 +9,27 @@ import { fetchWithAuth } from '../_libs/http-client';
 import type { IThemeDetail } from '../_types/theme';
 import { URLS } from '../constants';
 
+import { auth, unstable_update } from '@/auth';
+
 // 크리스마스 theme 하나만 출시해서 임시로 하드코딩
 const THEME_UID = '23bcf9f1-a487-11ef-9e7c-0237b5db447b';
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
+  const session = (await auth()) as (Session & { accessToken: string | null; refreshToken: string | null }) | null;
+  console.log(session);
   const refreshToken = async () => {
     try {
       await refreshTokenApi();
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
+      console.log(555, error);
+      if (session) {
+        session.accessToken = null;
+        session.refreshToken = null;
+        unstable_update(session).catch((err) => {
+          console.error('Error updating session:', err);
+        });
+      }
       redirect(URLS.getRootPage());
     }
   };
--- app/_libs/api/auth.ts
+++ app/_libs/api/auth.ts
@@ -1,14 +1,25 @@
 import type { Session } from 'next-auth';
 
-import { fetchWithAuth } from '@/app/_libs/http-client';
 import type { IAuthToken } from '@/app/_types/auth';
 import { URLS } from '@/app/constants';
-import { auth, unstable_update } from '@/auth';
+import { auth, signOut, unstable_update } from '@/auth';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://api.puzzletime.fun/api';
 
 export async function refreshToken(): Promise<IAuthToken> {
   const session = (await auth()) as (Session & { accessToken: string | null; refreshToken: string | null }) | null;
-  const response = await fetchWithAuth(URLS.refreshToken());
+  const response = await fetch(`${API_URL}${URLS.refreshToken()}`, {
+    method: 'POST',
+    headers: {
+      Cookie: `token=${session?.accessToken}`,
+      // Cookie: `token=${session?.refreshToken}`,
+    },
+  });
   if (!response.ok) {
+    const data: { code: string } = await response.json();
+    // if (data.code === 'ERR_TOKEN_REISSUE_REQUIRED') {
+    // }
+    console.log(333333, data, session);
     if (session) {
       session.accessToken = null;
       session.refreshToken = null;
@@ -19,6 +30,7 @@ export async function refreshToken(): Promise<IAuthToken> {
     throw new Error('Failed to refresh token');
   }
   const token = (await response.json()) as IAuthToken;
+  console.log(444444, token);
   if (session) {
     session.accessToken = token.appAccessToken;
     session.refreshToken = token.user.refreshToken;
